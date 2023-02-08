c39ce516eaf82d5322887dee4dbafc810143fb5b
thereal03 <79305551+thereal03@users.noreply.github.com>
2023-02-08 15:52:05 +0800

    2 files added

---
+++ prelims/exam/app.js
@@ -0,0 +1,5 @@
+var message = require('./greet.js');
+
+console.log(message.greet('Daryll'));
+console.log(message.congratulation('Daryll'));
+console.log(message.farewell('Daryll'));
---
+++ prelims/exam/greet.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return 'Hello , ' + name;
+}
+
+function congratulation(name) {
+  return 'Congratulations, ' + name;
+}
+
+function farewell(name) {
+  return 'Goodbye, ' + name;
+}
+
+module.exports = { greet, congratulation, farewell };
