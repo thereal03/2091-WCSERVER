cce99795bd9cd3452b6f7e27c50e7d40c568abc5
thereal03 <79305551+thereal03@users.noreply.github.com>
2023-03-13 12:52:50 +0800

    Add files via upload

---
+++ midterms/templating-library/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My EJS App' });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
